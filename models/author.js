995fba9dcd926c98b99415831a3ddfe73cf287e9
teotimepacreau <95431443+teotimepacreau@users.noreply.github.com>
2023-08-11 16:39:52 +0200

    finished author model and change arrow function to function in order to correctly use this

--- models/author.js
+++ models/author.js
@@ -22,10 +22,17 @@ const AuthorSchema = new Schema({
 })
 
 // SI JAMAIS L'AUTEUR N'A PAS DE PRENOM OU DE NOM ON RETOURNE UN FULLN STRING VIDE
-AuthorSchema.virtual("name").get(()=>{
+AuthorSchema.virtual("name").get(function (){//pas de arrow function pour que le this soit bien sur AuthorSchema
     let fullname = ""
     if(this.first_name && this.family_name){
         fullname = `${this.family_name}, ${this.first_name}`
     }
     return fullname
 });
+
+// Virtual pour returns the absolute URL required to get a particular instance of the model — we'll use the property in our templates whenever we need to get a link to a particular author.
+AuthorSchema.virtual("url").get(function(){//pas de arrow function pour que le this soit bien sur AuthorSchema
+    return `/catalog/author/${this._id}`
+})
+
+module.exports = mongoose.model("Author", AuthorSchema)
